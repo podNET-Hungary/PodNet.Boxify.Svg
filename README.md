e976857c7a399c8f3ffd1eb65013952c755f9a07
Gábor Szabó <szabo.gabor@podnet.hu>
2024-10-09 18:09:14 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,114 @@
+# PodNet.Boxify.Svg [![NuGet](https://img.shields.io/nuget/v/PodNet.Boxify.Svg)](https://www.nuget.org/packages/PodNet.Boxify.Svg/)
+An extension for [`PodNet.Boxify`](https://github.com/podNET-Hungary/PodNet.Boxify/)  (and [`PodNet.Boxify.Bmp`](https://github.com/podNET-Hungary/PodNet.Boxify.Bmp/)) that converts an SVG image to a rasterized format to be used for drawing box-drawings of images to text, files or terminals
+
+## Usage
+
+This package contains a single type: `PodNet.Boxify.Svg.SvgConverter`. This type is used to convert an SVG image to a bitmap, which will in turn be used to draw Unicode box-drawings of the originally vector-based image (now rasterized). However, it also takes a transient dependency on the core `PodNet.Boxify` package.
+
+```csharp
+using PodNet.Boxify;
+using PodNet.Boxify.Svg;
+
+// Get your graphics from wherever
+var podnet = """
+<svg width="1024" height="1024" viewBox="0 0 1024" xmlns="http://www.w3.org/2000/svg">
+    <path stroke-width="4" stroke="#fff" d="M133 0v570m0 143v311m19-518v518M326 0v333m0 246v445m67-597v508M548 0v471m0 146v407M593 0v133m0 530v361M723 0v363m0 249v412m56-692v508M950 0v540m0 145v339m15-548v548M0 151h50M0 496h15m271-252h198M224 600h346m93-276h361m-24-174h24m-3 345h3"/>
+    <path fill="#000" stroke-width="1" stroke="#fff" d="M192 192h640v640H192z"/>
+    <text font-family="Open Sans" x="192" y="512" font-size="192px" text-anchor="middle" fill="#fff">
+      <tspan x="512" y="472">pod</tspan><tspan x="512" y="672" font-weight="bold">NET</tspan>
+    </text>
+</svg>
+""";
+
+// You can set the Console's output encoding to properly display the characters. Usually only needed for high-res pixel palettes.
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// Choose from a range of palettes to use
+var palette = PixelPalette.Halves;
+
+// You can render only bitmaps. Use PodNet.Boxify.Bmp to wrap a simple System.Drawing.Bitmap, PodNet.Boxify.Svg to convert an
+// SVG to a wrapped System.Drawing.Bitmap, or supply your own by implementing this simple interface.
+IBoxBitmapSource bitmap = SvgConverter.Default.Convert(podnet, palette, maxRows: 40);
+
+var boxDrawing = Renderer.Default.Render(bitmap, palette);
+Console.WriteLine(boxDrawing);
+```
+
+Which outputs on the console:
+
+```cmd
+          █              █                █   █         █                 █     
+          █              █                █   █         █                 █     
+          █              █                █   █         █                 █     
+          █              █                █   █         █                 █     
+          █              █                █   █         █                 █     
+          █              █                █   ▀         █                 █   ▄▄
+          █              █                █             █                 █     
+          █    ▄▄▄▄▄▄▄▄▄▄█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄██▄▄▄▄▄▄▄▄▄▄▄▄█▄▄▄▄▄▄▄▄         █     
+          █    █                                                █         █     
+          █    █                                                █         █     
+          █    █                                                █         █     
+          █    █                                                █         █     
+          █    █                                                █▄▄▄▄▄▄▄▄▄█▄▄▄▄▄
+          █    █                                  ██            █         █     
+          █    █            ▄▄ ▄▄▄    ▄▄▄▄    ▄▄▄▄██            █         █     
+          █    █            ██▀▀▀██ ▄██▀▀▀█▄ ██▀▀▀██            █         █     
+          █    █            ██    ████    ██ ██   ██            █         █     
+          █    █            ██▄  ▄█ ▀██  ▄█▀ ██  ▄██            █         █     
+          █    █            ██▀▀▀▀    ▀▀▀▀▀   ▀▀▀▀▀▀            █         █▄    
+▀         █    █            ██                                  █         ██    
+          ██   █          ▄▄     ▄▄  ▄▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄▄▄         █         ██    
+          ██   █         ███▄    ██  ████████ ▀████████         █          █    
+          ▀█   █         █████   ██  ██           ██            █          █    
+           █   █         ███ ██▄ ██  ████████     ██            █          █    
+           █   █         ███  ▀█▄██  ██           ██            █          █    
+           █   █         ███   ▀███  ██▄▄▄▄▄▄▄    ██            █          █    
+           █   █          ▀▀     ▀▀  ▀▀▀▀▀▀▀▀     ▀▀            █          █    
+           █   █                                                █         ██    
+          ██   █                                                █         ██    
+          ██   █                                                █         ██    
+          ██   █                                                █         ██    
+          ██   █                                                █         ██    
+          ██   ▀▀▀▀▀▀▀▀▀▀█▀▀▀▀██▀▀▀▀▀▀▀▀▀▀██▀██▀▀▀▀▀▀▀▀▀█▀▀▀██▀▀▀         ██    
+          ██             █    █           █   █         █                 ██    
+          ██             █    █           █   █         █                 ██    
+          ██             █    █           █   █         █                 ██    
+          ██             █    ▀           █   █         █                 ██    
+          ██             █                █   █         █                 ██    
+          ██             █                █   █         █                 ██    
+          ██             █                █   █         █                 ██    
+```
+
+There are two overloads for the conversion available:
+```csharp
+IBoxBitmapSource Convert(string svgContent, int? maxWidth = null, int? maxHeight = null, float scaleX = 1f, float scaleY = 1f)
+IBoxBitmapSource Convert(string svgContent, PixelPalette palette, int? maxRows = null, int? maxColumns = null, float scaleX = 2f, float scaleY = 1f)
+```
+
+Note that this type does:
+- resizing given a `PodNet.Boxify.PixelPalette` to try and keep the aspect ratio of the rendered image, given the default approximately 1:2 aspect ratio of characters in many UI endpoints (clients), and given that the pixel palettes often define characters that are themselves a set of pixels (sextants, octants etc.),
+- SVG fixup; specifically, by default it is setting `fill` and `stroke` properties to `white` if not set on each element. You can override this behavior by inheriting from the class and overriding `SvgConverter.FixupSvg(SvgDocument svgDocument)`.
+
+Resizing works by scaling the image along the X and Y axis by `scaleX` and `scaleY`, then resizing as needed while keeping the aspect ratio and taking into account the `maxWidth` and `maxHeight` to fit. This essentially produces a stretched/squashed image that will be normalized during the rendering process.
+
+The second overload (which calls into the first one) takes a `PixelPalette` to remove the "calculation" needed on the part of the user. If you're rendering the box-drawing elsewhere, you can configure the `scaleX` and `scaleY` parameters according to your needs, for example, to draw square-shaped characters when using some specific fonts, or when using terminals/IDEs/viewers with character outputs set to other than 1:2 aspect ratio characters.
+
+The converter also manipulates the provided image to do some **"fixup"**. Specifically, by default, it is setting `fill` and `stroke` properties to `white` where not set on each element. This is so that SVGs that would otherwise be transparent can be rendered on a "transparent" background as monochrome images. You can override this behavior by inheriting from the class and overriding `SvgConverter.FixupSvg(SvgDocument svgDocument)`. Also consider the options provided by `PodNet.Boxify.PixelAnalyzer` (which take into account the alpha channel, HSB brightness, or both), or supply your own implementation by inheriting from it to determine which pixels should be considered "set" (monochrome) or how bright (grayscale images).
+
+You can also render colorized images (including full RGB gradients), as `PodNet.Boxify` already support that.
+
+For more details on how to use [`PodNet.Boxify`](https://github.com/podNET-Hungary/PodNet.Boxify/), including colorization, see the repo at https://github.com/podNET-Hungary/PodNet.Boxify/.
+
+This package depends on the [https://github.com/svg-net/SVG] library, which is used to render the image to a `System.Drawing.Bitmap` from [`System.Drawing.Common`](https://www.nuget.org/packages/System.Drawing.Common/). Do note that rendering SVGs outside of their "native habitats" (browsers) can lead to some unexpected results, especially when using advanced features like new CSS features or masking in SVG.
+
+## Contributing and Support
+
+This project is intended to be widely usable, but no warranties are provided. If you want to contact us, feel free to do so in the org's [[Discussions](https://github.com/orgs/podNET-Hungary/discussions/)], at our website at [podnet.hu](https://podnet.hu), or find us anywhere from [LinkedIn](https://www.linkedin.com/company/podnet-hungary/) to [Meetup](https://www.meetup.com/budapest-net-meetup/), [YouTube](https://www.youtube.com/@podNET) or [X](https://twitter.com/podNET_Hungary).
+
+Any kinds of contributions from issues to PRs and open discussions are welcome!
+
+Don't forget to give us a ⭐ if you like this repo (it's free to give kudos!) or share it on socials!
+
+## Sponsorship
+
+If you're using our work or like what you see, consider supporting us. Every bit counts. 🙏 [See here for more info.](https://github.com/podNET-Hungary/PodNet.NuGet.Core/blob/main/src/PodNet.NuGet.Core/build/SPONSORS.md)
---
+++ src/Boxify.Svg/SvgConverter.cs
@@ -0,0 +1,133 @@
+﻿using PodNet.Boxify.Bmp;
+using Svg;
+using System.Drawing;
+
+namespace PodNet.Boxify.Svg;
+
+/// <summary>
+/// Handles converting an SVG image to a box-art-compatible source for <see cref="Renderer"/>.
+/// You can inherit from this class to override different aspects of the conversion process.
+/// </summary>
+/// <remarks>
+/// <list type="bullet">
+/// <item>Note that this is not a framework component for Boxify but a convenience class to be invoked directly by user code.</item>
+/// <item>Also do note that converting a bitmap to the corresponding box-art doesn't do resizing/aspect ratio conversion/pixel
+/// ratio conversion on its own. You can use the <a href="https://www.nuget.org/packages/Svg">Svg</a> package to do image
+/// manipulation on SVGs (besides this class), or some functions from <a href="https://www.nuget.org/packages/System.Drawing.Common">
+/// System.Drawing.Common</a> as they are transiently referenced through the current package.</item>
+/// </list>
+/// </remarks>
+public class SvgConverter
+{
+    /// <summary>The protected constructor for the converter. The default implementation is stateless,
+    /// so users should prefer to use the <see cref="Default"/> instance.</summary>
+    protected SvgConverter() { }
+
+    /// <summary>The default converter instance.</summary>
+    public static SvgConverter Default { get; } = new();
+
+    /// <summary>Create a <see cref="IBoxBitmapSource"/> wrapper around a rasterized image from the provided SVG.</summary>
+    /// <remarks>The default implementation calls, in order:
+    /// <list type="bullet">
+    /// <item><see cref="CreateSvg(string)"/></item>
+    /// <item><see cref="FixupSvg(SvgDocument)"/></item>
+    /// <item><see cref="RasterizeSvg(SvgDocument, int?, int?, float, float)"/></item>
+    /// </list>
+    /// </remarks>
+    /// <param name="svgContent">The full content of the SVG (XML).</param>
+    /// <param name="maxWidth">The maximum width the bitmap should be rendered in (while keeping the aspect ratio).</param>
+    /// <param name="maxHeight">The maximum width the bitmap should be rendered in (while keeping the aspect ratio).</param>
+    /// <param name="scaleX">Indicates the horizontal scaling to use when rasterizing the image. Useful for when characters
+    /// represent multiple pixels (or fractions) in width and when rendered characters are not square-shaped.</param>
+    /// <param name="scaleY">Indicates the vertical scaling to use when rasterizing the image. Useful for when characters
+    /// represent multiple pixels (or fractions) in height and when rendered characters are not square-shaped.</param>
+    public virtual IBoxBitmapSource Convert(string svgContent, int? maxWidth = null, int? maxHeight = null, float scaleX = 1f, float scaleY = 1f)
+    {
+        var svgDocument = CreateSvg(svgContent);
+        FixupSvg(svgDocument);
+        return RasterizeSvg(svgDocument, maxWidth, maxHeight, scaleX, scaleY);
+    }
+
+    /// <summary>Create a <see cref="IBoxBitmapSource"/> wrapper around a rasterized image from the provided SVG.</summary>
+    /// <remarks>This is a convenience method for applying scaling transforms using the provided <paramref name="palette"/>.
+    /// The dimensions and scaling factors are derived appropriately from the provided palette and parameters. For more info,
+    /// see the <see cref="Convert(string, int?, int?, float, float)"/> overload.<br/>
+    /// Default scaling is 2f for <paramref name="scaleX"/> and 1f for <paramref name="scaleY"/>, because many monospace
+    /// renderers (like IDEs) use close to approximately 1x2 sized rectangles for the monospaced characters.
+    /// </remarks>
+    /// <param name="svgContent">The full content of the SVG (XML).</param>
+    /// <param name="maxRows">The maximum number of rows in the output box-art.</param>
+    /// <param name="maxColumns">The maximum number of columns (characters, can be multiple for each code point) in the
+    /// output box-art.</param>
+    /// <param name="scaleX">Additional scaling to use on the X axis, besides the one provided by <see cref="PixelPalette.PixelWidth"/>.</param>
+    /// <param name="scaleY">Additional scaling to use on the Y axis, besides the one provided by <see cref="PixelPalette.PixelHeight"/>.</param>
+    /// <param name="palette">The palette to derive the scaling information from.</param>
+    public virtual IBoxBitmapSource Convert(string svgContent, PixelPalette palette, int? maxRows = null, int? maxColumns = null, float scaleX = 2f, float scaleY = 1f)
+        => Convert(svgContent, maxColumns * palette.PixelWidth, maxRows * palette.PixelHeight, palette.PixelWidth * scaleX, palette.PixelHeight * scaleY);
+
+    /// <summary>Creates an <see cref="SvgDocument"/> from the provided string content.</summary>
+    /// <remarks>The default implementation calls <see cref="SvgDocument.FromSvg{T}(string)"/>.</remarks>
+    /// <param name="svgContent">The full string (including XML header) of the SVG.</param>
+    /// <returns>The parsed SVG document.</returns>
+    protected virtual SvgDocument CreateSvg(string svgContent)
+        => SvgDocument.FromSvg<SvgDocument>(svgContent);
+
+    /// <summary>A shorthand for the default <see cref="SvgColourServer"/> using <see cref="Color.White"/>.
+    /// Used by the default <see cref="FixupSvg(SvgDocument)"/>.</summary>
+    protected static SvgColourServer White { get; } = new(Color.White);
+
+    /// <summary>Prepares the SVG to be rasterized by making changes to the SVG structure. This is essential because most SVG
+    /// defaults are optimal for web-based rendering, and not box-art rendering as required by Boxify.</summary>
+    /// <remarks>The default implementation sets all <see cref="SvgElement"/> descendants of the <paramref name="svgDocument"/>
+    /// to have <see cref="White"/> <see cref="SvgElement.Fill"/>, <see cref="SvgElement.Color"/> and <see cref="SvgElement.Stroke"/> 
+    /// if not otherwise explicitly set to <see cref="SvgPaintServer.None"/>. <a href="https://svg-net.github.io/SVG/articles/Faq.html#how-to-render-an-svg-image-to-a-single-color-bitmap-image">See source for details.</a></remarks>
+    /// <param name="svgDocument">The document to fix up.</param>
+    protected virtual void FixupSvg(SvgDocument svgDocument)
+    {
+        foreach (var element in svgDocument.Descendants())
+        {
+            if (element.Fill == SvgPaintServer.NotSet)
+                element.Fill = White;
+            if (element.Stroke == SvgPaintServer.NotSet)
+                element.Stroke = White;
+        }
+    }
+
+    /// <summary>
+    /// Calculates the target dimensions from <paramref name="svgDocument"/> for the rasterized image given either, neither
+    /// or both of the max width and height, while keeping the image's intrinsic aspect ratio, but accounting for the possibility
+    /// of scaling on the X and Y axes for non-square shaped pixels or custom render targets.
+    /// </summary>
+    /// <param name="svgDocument">The document to calculate the target values from.</param>
+    /// <param name="maxWidth">The expected maximum width of the rasterized image. Can be null if ignored.</param>
+    /// <param name="maxHeight">The expected maximum height of the rasterized image. Can be null if ignored.</param>
+    /// <param name="scaleX">Scales the document on the X axis. The original aspect ratio is skewed on the X axis if X != 1f.</param>
+    /// <param name="scaleY">Scales the document on the Y axis. The original aspect ratio is skewed on the Y axis if Y != 1f.</param>
+    /// <returns>The target width and height of the image to be rasterized. Either, neither or both can be ZERO if ignored.</returns>
+    protected virtual (int TargetWidth, int TargetHeight) GetTargetDimensions(SvgDocument svgDocument, int? maxWidth, int? maxHeight, float scaleX, float scaleY)
+    {
+        if (scaleX <= 0f)
+            throw new ArgumentOutOfRangeException(nameof(scaleX), scaleX, "Value should be positive.");
+        if (scaleY <= 0f)
+            throw new ArgumentOutOfRangeException(nameof(scaleY), scaleY, "Value should be positive.");
+        var (scaledWidth, scaledHeight) = (svgDocument.Width * scaleX, svgDocument.Height * scaleY);
+        var aspectRatio = scaledWidth / scaledHeight;
+        var (w, h) = (maxWidth ?? (int)scaledWidth, maxHeight ?? (int)scaledHeight);
+        return (w / aspectRatio <= h) ? (w, (int)(w / aspectRatio)) : ((int)(h * aspectRatio), h);
+    }
+
+    /// <summary>Rasterizes the given <paramref name="svgDocument"/> to a <see cref="BitmapSource"/>.</summary>
+    /// <remarks>The default implementation calls <see cref="GetTargetDimensions(SvgDocument, int?, int?, float, float) "/>
+    /// and <see cref="SvgDocument.Draw(int, int)"/>.</remarks>
+    /// <param name="svgDocument">The image to rasterize.</param>
+    /// <param name="maxWidth">The max width to fit the resulting image into. Can be null if ignored.</param>
+    /// <param name="maxHeight">The max height to fit the resulting image into. Can be null if ignored.</param>
+    /// <param name="scaleX">Scales the document on the X axis. The original aspect ratio is skewed on the X axis if X != 1f.</param>
+    /// <param name="scaleY">Scales the document on the Y axis. The original aspect ratio is skewed on the Y axis if Y != 1f.</param>
+    /// <returns>The rasterized image, wrapped in a <see cref="BitmapSource"/>.</returns>
+    protected virtual IBoxBitmapSource RasterizeSvg(SvgDocument svgDocument, int? maxWidth, int? maxHeight, float scaleX, float scaleY)
+    {
+        var (width, height) = GetTargetDimensions(svgDocument, maxWidth, maxHeight, scaleX, scaleY);
+        return new BitmapSource(svgDocument.Draw(width, height));
+    }
+}
